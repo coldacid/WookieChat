f4996889f63ace580d5612df33326821b234799a
geit <geit@a134683b-0014-45c7-9bf7-2215d65c34c1>
2013-06-15 23:05:46 +0000

    added missing files
    git-svn-id: https://svn.code.sf.net/p/wookiechat/code/trunk@127 a134683b-0014-45c7-9bf7-2215d65c34c1

---
+++ muiclass_connectedlist.c
@@ -0,0 +1,168 @@
+/*
+    The contents of this file are subject to the AROS Public License Version 1.1
+    (the "License"); you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at http://www.aros.org/license.html
+
+    Software distributed under the License is distributed on an "AS IS" basis,
+    WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+    for the specific language governing rights and limitations under the License.
+*/
+
+/*
+** muiclass_connectedlist.c
+*/
+
+#include <libraries/mui.h>
+#include <prefs/prefhdr.h>
+#include <proto/muimaster.h>
+#include <proto/intuition.h>
+#include <proto/utility.h>
+#include <proto/exec.h>
+#include <SDI_hook.h>
+
+#include <mui/NList_mcc.h>
+#include <mui/NListview_mcc.h>
+#include <string.h>
+
+#include "locale.h"
+#include "muiclass.h"
+#include "muiclass_connectedlist.h"
+#include "version.h"
+
+/*************************************************************************/
+
+/*
+** gadgets used by this class
+*/
+
+enum
+{
+GID_AREXXPORT = 0,
+GID_LAST
+};
+
+/*
+** data used by this class
+*/
+
+struct mccdata
+{
+	Object                *mcc_ClassObjects[ GID_LAST ];
+};
+
+/*************************************************************************/
+
+/* /// OM_New()
+**
+*/
+
+/*************************************************************************/
+
+static ULONG OM_New( struct IClass *cl, Object *obj, struct opSet *msg UNUSED )
+{
+Object *objs[ GID_LAST ];
+
+	if( (obj = (Object *)DoSuperNew( cl, obj,
+		TAG_DONE ) ) ) {
+
+		struct mccdata *mccdata = INST_DATA( cl, obj );
+
+		CopyMem( &objs[0], &mccdata->mcc_ClassObjects[0], sizeof( mccdata->mcc_ClassObjects));
+
+		return( (ULONG) obj );
+    }
+	return( (ULONG) NULL );
+}
+/* \\\ */
+/* /// OM_Dispose()
+**
+*/
+
+/*************************************************************************/
+
+static ULONG OM_Dispose( struct IClass *cl, Object *obj, Msg msg )
+{
+//struct mccdata *mccdata = INST_DATA( cl, obj );
+
+	return( DoSuperMethodA( cl, obj, msg ) );
+}
+/* \\\ */
+/* /// OM_Set()
+**
+*/
+
+/*************************************************************************/
+
+static ULONG OM_Set( struct IClass *cl, Object *obj, struct opSet *msg )
+{
+#if 0
+struct mccdata *mccdata = INST_DATA( cl, obj );
+struct TagItem *tag;
+struct TagItem *tstate;
+
+	for( tstate = msg->ops_AttrList ; ( tag = NextTagItem( &tstate ) ) ; ) {
+		ULONG tidata = tag->ti_Data;
+        switch( tag->ti_Tag ) {
+		}
+    }
+#endif
+	return( DoSuperMethodA( cl, obj,(Msg) msg ) );
+}
+/* \\\ */
+
+/*
+** Dispatcher, init and dispose
+*/
+
+/* /// MCC_ConnectedList_Dispatcher()
+**
+*/
+
+/*************************************************************************/
+
+DISPATCHER(MCC_ConnectedList_Dispatcher)
+{
+    switch (msg->MethodID)
+    {
+		case OM_NEW                          : return( OM_New                           ( cl, obj, (APTR) msg ) );
+		case OM_DISPOSE                      : return( OM_Dispose                       ( cl, obj, (APTR) msg ) );
+
+		case OM_SET                          : return( OM_Set                           ( cl, obj, (APTR) msg ) );
+
+/* application specific methods */
+
+    }
+	return( DoSuperMethodA( cl, obj, msg ) );
+
+}
+/* \\\ */
+/* /// MCC_ConnectedList_InitClass()
+**
+*/
+
+/*************************************************************************/
+
+ULONG MCC_ConnectedList_InitClass( void )
+{
+	appclasses[ CLASSID_CONNECTEDLIST ] = MUI_CreateCustomClass( NULL, (ClassID) MUIC_NList, NULL, sizeof( struct mccdata ) ,  (APTR) ENTRY(MCC_ConnectedList_Dispatcher) );
+	return( appclasses[ CLASSID_CONNECTEDLIST ] ? MSG_ERROR_NOERROR : MSG_ERROR_UNABLETOSETUPMUICLASS );
+}
+/* \\\ */
+/* /// MCC_ConnectedList_DisposeClass()
+**
+*/
+
+/*************************************************************************/
+
+void MCC_ConnectedList_DisposeClass( void )
+{
+	if( appclasses[ CLASSID_CONNECTEDLIST ] ) {
+		MUI_DeleteCustomClass( appclasses[ CLASSID_CONNECTEDLIST ] );
+		appclasses[ CLASSID_CONNECTEDLIST ] = NULL;
+    }
+}
+/* \\\ */
+
+
+
+
